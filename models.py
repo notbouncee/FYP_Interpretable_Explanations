ab4a681fcce064d15ca9d80f3804d272939911c9
Vince <129482642+notbouncee@users.noreply.github.com>
2025-11-10 03:26:34 +0800

    Added Interim Report, Edited mainCora code

--- models.py
+++ models.py
@@ -3,113 +3,81 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 from typing import cast
-from torch.nn import Linear
-from torch_geometric.nn import global_mean_pool
 from torch_geometric.nn import SAGEConv
+from torch_geometric.nn import GCNConv
+from torch.nn import Linear, BatchNorm1d
 
-# GCN basic operation single layer
-class GraphConv(nn.Module):
-    def __init__(
-        self,
-        input_dim,
-        output_dim,
-        add_self=False,
-        normalize_embedding=False,
-        dropout=0.0,
-        bias=True,
-        device=None,
-        att=False,
-    ):
-        super(GraphConv, self).__init__()
-        self.att = att
-        self.add_self = add_self
-        self.dropout = dropout
-        if dropout > 0.001:
-            self.dropout_layer = nn.Dropout(p=dropout)
-        self.normalize_embedding = normalize_embedding
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        if device is None:
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.device = device
+class GCN(torch.nn.Module):
+    def __init__(self, num_features, hidden_channels, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(num_features, hidden_channels)
+        self.bn1 = BatchNorm1d(hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.bn2 = BatchNorm1d(hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.bn3 = BatchNorm1d(hidden_channels)
+        self.conv4 = GCNConv(hidden_channels, hidden_channels)
+        self.bn4 = BatchNorm1d(hidden_channels)
+        self.out = Linear(hidden_channels, num_classes)
 
-        self.weight = nn.Parameter(torch.empty(input_dim, output_dim, device=device))
-        if add_self:
-            self.self_weight = nn.Parameter(
-                torch.empty(input_dim, output_dim, device=device)
-            )
-        if att:
-            self.att_weight = nn.Parameter(torch.empty(input_dim, input_dim, device=device))
-        if bias:
-            self.bias = nn.Parameter(torch.empty(output_dim, device=device))
-        else:
-            self.bias = None
+    def forward(self, x, edge_index):
+        # First Message Passing Layer (Transformation)
+        x = self.conv1(x, edge_index)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
 
-        # Initialize parameters after device placement
-        init.xavier_uniform_(self.weight)
-        if add_self:
-            init.xavier_uniform_(self.self_weight)
-        if att:
-            init.xavier_uniform_(self.att_weight)
-        if self.bias is not None:
-            init.zeros_(self.bias)
+        # Second Message Passing Layer
+        x = self.conv2(x, edge_index)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
 
-        # self.softmax = nn.Softmax(dim=-1)
+        # Third Message Passing Layer
+        x = self.conv3(x, edge_index)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
 
-    def forward(self, x, adj):
-        if self.dropout > 0.001:
-            x = self.dropout_layer(x)
-        # deg = torch.sum(adj, -1, keepdim=True)
-        if self.att:
-            x_att = torch.matmul(x, self.att_weight)
-            # import pdb
-            # pdb.set_trace()
-            att = x_att @ x_att.permute(0, 2, 1)
-            # att = self.softmax(att)
-            adj = adj * att
+        # Fourth Message Passing Layer
+        x = self.conv4(x, edge_index)
+        x = self.bn4(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
 
-        y = torch.matmul(adj, x)
-        y = torch.matmul(y, self.weight)
-        if self.add_self:
-            self_emb = torch.matmul(x, self.self_weight)
-            y += self_emb
-        if self.bias is not None:
-            y = y + self.bias
-        if self.normalize_embedding:
-            y = F.normalize(y, p=2, dim=2)
-            # print(y[0][0])
-        return y, adj
-
+        # Output layer
+        x = self.out(x)
+        return x
 
 
 class GraphSAGE(torch.nn.Module):
-    def __init__(self, dataset, num_layers, hidden):
+    def __init__(self, dataset, num_layers, hidden, dropout: float = 0.5):
         super().__init__()
-        self.conv1 = SAGEConv(dataset.num_features, hidden)
-        self.convs = torch.nn.ModuleList()
-        for i in range(num_layers - 1):
-            self.convs.append(SAGEConv(hidden, hidden))
-        self.lin1 = Linear(hidden, hidden)
-        self.lin2 = Linear(hidden, dataset.num_classes)
+        if num_layers < 2:
+            raise ValueError("GraphSAGE requires at least two layers for node classification.")
 
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(dataset.num_features, hidden))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden, hidden))
+        self.convs.append(SAGEConv(hidden, dataset.num_classes))
+        self.dropout = dropout
 
     def reset_parameters(self):
-        self.conv1.reset_parameters()
         for conv in self.convs:
             cast(SAGEConv, conv).reset_parameters()
-        self.lin1.reset_parameters()
-        self.lin2.reset_parameters()
 
-
-    def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
-        x = F.relu(self.conv1(x, edge_index))
-        for conv in self.convs:
-            x = F.relu(conv(x, edge_index))
-        x = global_mean_pool(x, batch)
-        x = F.relu(self.lin1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.lin2(x)
-        return F.log_softmax(x, dim=-1)
+    def forward(self, data_or_x, edge_index=None):
+        if edge_index is None:
+            data = data_or_x
+            x, edge_index = data.x, data.edge_index
+        else:
+            x = data_or_x
+        for conv in self.convs[:-1]:
+            x = conv(x, edge_index)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, edge_index)
+        return x
 
 
